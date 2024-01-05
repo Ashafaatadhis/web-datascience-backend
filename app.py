b094193bbb7bcf71e4e2eb3af84e0d386620fff0
ashafaatadhis <skullreaper387@gmail.com>
2024-01-05 20:02:06 +0700

    feat: finishing api

--- app.py
+++ app.py
@@ -33,12 +33,51 @@ def get_data():
         return GetDto.getDto([], False), 500
     # return jsonify({"data": json.loads(anime[awal:akhir].to_json(orient = 'records'))})
 
+@app.route('/api/get/<name>')
+def get_data_detail(name):
+    
+    # Load your pickle file here
+    anime = pickle.load(open("./movies_list.pkl", "rb"))
+    try:
+        d = json.loads(anime[anime["Name"] == name].to_json(orient = 'records'))[0]
+        return GetDto.getDto(d, True), 200
+    except:
+        return GetDto.getDto([], False), 500
+    # return jsonify({"data": json.loads(anime[awal:akhir].to_json(orient = 'records'))})
+
 @app.route("/api/anime")
+async def detail():
+    try:
+        s = request.args.get('s')
+        print(s)
+        anime = await get_list_anime(s)
+        return GetDto.getDto(anime, True), 200
+    except:
+        return GetDto.getDto([], False), 404
+    
+@app.route("/api/anime/stream")
+async def stream():
+    try:
+        s = request.args.get('s')
+        anime = await get_stream(s)
+        print("ini", anime)
+        return GetDto.getDto(anime, True), 200
+    except Exception as e:
+        print(e)
+        return GetDto.getDto([], False), 404
+    
+@app.route("/api/anime/detail")
 async def episode():
-    s = request.args.get('s')
-    print(s)
-    anime = await get_episodes(s)
-    return GetDto.getDto(anime, True), 200
+    try:
+        s = request.args.get('s')
+      
+        eps = await get_episodes(s) 
+       
+        return GetDto.getDto(eps[::-1], True), 200
+    except:
+        return GetDto.getDto([], False), 404
+
+
 
 @app.route("/api/top")
 def get_top_anime():
@@ -61,51 +100,102 @@ def get_recommend():
         return GetDto.getDto([], False), 500
     # return jsonify({"data": json.loads(rec.to_json(orient="records"))})
 
-
-# def scrap():
-#     BeautifulSoup.
     
- 
-    
-async def get_episodes(title):
+async def get_stream(url):
     browser = await launch(handleSIGINT=False,handleSIGTERM=False,handleSIGHUP=False)
     page = await browser.newPage()
-    await page.goto('https://tv1.kuronime.vip/')
-    await page.type('.search-live', title)
-    await page.click("#submit")
-    await page.waitForNavigation()
-    # element = await page.querySelector('div.bsx a')
-    # title = await page.evaluate('(element) => element.href', element)
-    search =  await page.evaluate("""() => {
-    let arr = [];
-    let element = document.querySelectorAll('div.bsx a');
-    element.forEach((el)=>{
-                             
-        arr.push(el.href);                   
-    });
-    return arr;
+    try:
+        await page.goto(url)
     
-                             }
-                             """)
+        await page.click("#play-asu")
+        await page.waitForSelector('#select-mirror-container select#mirrorList')
+        await page.select('select#mirrorList', 'v720p,krakenfiles')
+         
+        await page.waitForFunction('document.getElementById("iframedc").src != "about:blank"')
+        
+        
+        search = await page.evaluate("""() => {
+       
+        let element = document.querySelector('iframe#iframedc');
+        return element.src;
+        
+                                }
+                                """)
+       
+ 
+      
+       
+    except Exception as e:
+        print(e)
+    finally:
+        print("TERTUTUP")
+        await browser.close()
+    return search
+
+async def get_episodes(s):
+    browser = await launch(handleSIGINT=False,handleSIGTERM=False,handleSIGHUP=False)
+    page = await browser.newPage()
+    print("Ini", s)
+    try:
+        await page.goto(s)
+       
+       
+ 
+        p =  await page.evaluate("""() => {
+        let arr = [];
+        let element = document.querySelectorAll('.lchx a');
+        element.forEach((el)=>{
+                                
+            arr.push(el.href);                   
+        });
+        return arr;
+        
+                                }
+                                """)
+        # element = await page.querySelectorAll("span.lchx")
+        # p = await page.evaluate("""(element) => element""", element)
+        
+        await page.screenshot({'path': 'screenshot.png'})
+
+    except Exception as e:
+        print(e)
+    finally:
+        print("TERTUTUP")
+        await browser.close()
+    return p 
+
+async def get_list_anime(title):
+    browser = await launch(handleSIGINT=False,handleSIGTERM=False,handleSIGHUP=False)
+    page = await browser.newPage()
+    try:
+        await page.goto('https://tv1.kuronime.vip/')
+        await page.type('.search-live', title)
+        await page.click("#submit")
+        await page.waitForNavigation()
+        # element = await page.querySelector('div.bsx a')
+        # title = await page.evaluate('(element) => element.href', element)
+        search =  await page.evaluate("""() => {
+        let arr = [];
+        let element = document.querySelectorAll('div.bsx a');
     
-    await page.goto(search[-1])
-    p =  await page.evaluate("""() => {
-    let arr = [];
-    let element = document.querySelectorAll('.lchx a');
-    element.forEach((el)=>{
-                             
-        arr.push(el.href);                   
-    });
-    return arr;
+        element.forEach((el, index)=>{
+            let img = document.querySelectorAll('div.bsx div.limit img.lazyloaded')[index];
+            let title = document.querySelectorAll('div.bsx div.tt h4')[index];
+                            
+            let temp = {title: title.textContent, link:el.href, img: img.src};
+                                            
+            arr.push(temp);                   
+        });
+        return arr;
+        
+                                }
+                                """)    
+       
     
-                             }
-                             """)
-    # element = await page.querySelectorAll("span.lchx")
-    # p = await page.evaluate("""(element) => element""", element)
-    
-    await page.screenshot({'path': 'screenshot.png'})
-    await browser.close()
-    return p
+    finally:
+        print("TERTUTUP")
+        await browser.close()
+    return search
 
 
 def recommend(title):
